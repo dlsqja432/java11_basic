f03645a33a510b185bd874ebf891f62089a29bd3
dlsqja432 <dlsqja432@naver.com>
2024-02-22 00:28:48 +0900

    baekjoon algorithm

--- algorithmtest/src/array/Ex6.java
+++ algorithmtest/src/array/Ex6.java
@@ -9,8 +9,26 @@ public class Ex6 {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
 		int m = scan.nextInt();
+		int[] arr = new int[n];
+		int tmp;
 		
+		for(int i=1; i<=n; i++) {
+			arr[i-1] = i;
+		}
 		
+		for(int i=0; i<m; i++) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			tmp = arr[a-1];
+			arr[a-1] = arr[b-1];
+			arr[b-1] = tmp;
+		}
+		
+		for(int result : arr) {
+			System.out.print(result+ " ");
+		}
+		
+		scan.close();
 	}
 
 }
