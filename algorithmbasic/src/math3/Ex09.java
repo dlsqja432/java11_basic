54081bf0dac135977adad2385956e3d5746123ab
dlsqja432 <dlaqja432@naver.com>
2024-03-26 19:32:36 +0900

    java11 algorithm basic

--- algorithmbasic/src/math3/Ex09.java
+++ algorithmbasic/src/math3/Ex09.java
@@ -8,7 +8,6 @@ import java.util.StringTokenizer;
 // 11053번 가장 긴 증가하는 부분 수열
 public class Ex09 {
 
-//	점화식 : 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
@@ -19,17 +18,18 @@ public class Ex09 {
 		for(int i=1; i<=n; i++) {
 			int a = Integer.parseInt(st.nextToken());
 			arr[i] = a;
+			dp[i] = 1;
 		}
 		
-		dp[1] = 1;
-		for(int i=1; i<n; i++) {
-			if(arr[i+1] > arr[i]) {
-				dp[i+1] = dp[i]+1;
-			} else {
-				dp[i+1] = dp[i];
+		int max = 1;
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<i; j++) {
+				if(arr[i] > arr[j]) {
+					dp[i] = Math.max(dp[i], dp[j]+1);
+				}
 			}
-		}
-		
-		System.out.println(dp[n]);
+			max = Math.max(max, dp[i]);
+ 		}
+		System.out.println(max);
 	}	
 }
